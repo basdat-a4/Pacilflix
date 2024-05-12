7230ef4399fe6ac20e60e01412d976ff3f1ad1b8
Indira <indira.rahmah38@gmail.com>
2024-05-12 17:26:44 +0700

    Penambahan url daftar unduhan dan favorit

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from main.views import show_main
+from . import views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('unduhan/', views.daftar_unduhan, name='daftar_unduhan'),
+    path('favorit/', views.daftar_favorit, name='daftar_favorit'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     return render(request, "mainmenu.html")
+
+def daftar_unduhan(request):
+    # context
+    return render(request, 'daftar_unduhan.html')
+
+def daftar_favorit(request):
+    # context
+    return render(request, 'daftar_favorit.html' )
