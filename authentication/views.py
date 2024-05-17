e457c6f21cd17d0eee40f09f4726987a7be9258a
Fadeeaal <rakha.fadil@ui.ac.id>
2024-05-17 12:43:32 +0700

    Update : minor update

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib import messages  
 from django.db import connection
 import datetime
-
-from django.urls import reverse
 
 cursor = connection.cursor()
 
@@ -23,17 +21,19 @@ def show_login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         cursor.execute("SET search_path TO pacilflix;")
-        cursor.execute("SELECT * FROM PENGGUNA WHERE username = %s AND password = %s", [username, password])
+        cursor.execute(f"SELECT * FROM PENGGUNA P WHERE P.username = '{username}' AND P.password = '{password}'")
         users = cursor.fetchone()
 
         if users is not None:
-            response = HttpResponseRedirect(reverse("tayangan:show_tayangan"))
+            request.session['username'] = users[0]
+            response = redirect('tayangan:show_tayangan')
             response.set_cookie('username', users[0])
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
         else:
             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    return render(request, 'login.html')
+    context = {}
+    return render(request, 'login.html', context)
 
 def show_register(request):
     if request.method == "POST":
@@ -46,16 +46,15 @@ def show_register(request):
         if cursor.fetchone() is not None:
             messages.error(request, f"Username {username} sudah tersedia.")
             return render(request, 'register.html', {'form': request.POST})
-        
-        response = HttpResponseRedirect(reverse("authentication:show_login"))
-        cursor.execute("INSERT INTO PENGGUNA (username, password, negara_asal) VALUES (%s, %s, %s)", [username, password, negara])
+
+        cursor.execute("INSERT INTO pengguna (username, password, negara_asal) VALUES (%s, %s, %s)", [username, password, negara])
         messages.success(request, 'Your account has been successfully created!')
-        return response
+        return redirect('authentication:show_login')
+
     return render(request, 'register.html')
 
-@login_required_custom
 def logout_user(request):
-    response = HttpResponseRedirect(reverse("authentication:show_main"))
+    response = redirect('authentication:show_main')
     for cookie in request.COOKIES:
         response.delete_cookie(cookie)
     return response
