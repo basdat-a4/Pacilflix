1256c38c2ad05937449374b983e1f47b839b5564
Fadeeaal <rakha.fadil@ui.ac.id>
2024-05-17 13:54:42 +0700

    Update : minor update

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import redirect
-from django.contrib import messages  
+from django.contrib import messages
 from django.db import connection
 import datetime
 
-
+# Custom decorator to check login
 def login_required_custom(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
         if 'username' not in request.COOKIES:
@@ -12,29 +11,32 @@ def login_required_custom(view_func):
         return view_func(request, *args, **kwargs)
     return _wrapped_view_func
 
+# View to show the main menu
 def show_main(request):
     return render(request, "mainmenu.html")
 
+# View to handle login
 def show_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        
         with connection.cursor() as cursor:
             cursor.execute("SET search_path TO Pacilflix;")
             cursor.execute("SELECT * FROM PENGGUNA WHERE username = %s AND password = %s", [username, password])
             users = cursor.fetchone()
 
         if users is not None:
-            request.session['username'] = users[0]
             response = redirect('tayangan:show_tayangan')
             response.set_cookie('username', users[0])
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
         else:
             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
+    
+    return render(request, 'login.html')
 
+# View to handle user registration
 def show_register(request):
     if request.method == "POST":
         username = request.POST.get('username')
@@ -47,12 +49,16 @@ def show_register(request):
             if cursor.fetchone() is not None:
                 messages.error(request, f"Username {username} sudah tersedia.")
                 return render(request, 'register.html', {'form': request.POST})
+
             cursor.execute("INSERT INTO PENGGUNA (username, password, negara_asal) VALUES (%s, %s, %s)", [username, password, negara])
         
         messages.success(request, 'Your account has been successfully created!')
         return redirect('authentication:show_login')
+
     return render(request, 'register.html')
 
+# View to handle user logout
+@login_required_custom
 def logout_user(request):
     response = redirect('authentication:show_main')
     for cookie in request.COOKIES:
