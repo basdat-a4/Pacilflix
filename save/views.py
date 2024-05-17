603ca90d1d035285df495c3950f3e9b15a601aa8
Indira <indira.rahmah38@gmail.com>
2024-05-17 20:23:21 +0700

    perbaruan daftar favorit dan unduhan, views

--- save/views.py
+++ save/views.py
@@ -9,29 +9,25 @@ def daftar_unduhan(request):
                            SELECT id_tayangan, username, judul, timestamp
                            FROM TAYANGAN_TERUNDUH
                            WHERE username = %s
-                           """, [username])
+                           AND id = id_tayangan""", [username])
             unduhan_list = cursor.fetchall()
         return render(request, 'daftar_unduhan.html', {'unduhan_list': unduhan_list, 'user': request.user})
 
 
 def daftar_favorit(request):
-    if request.user.is_authenticated:
-        username = request.user.username
+        username = request.COOKIES["username"]
         with connection.cursor() as cursor:
             cursor.execute("""
                 SELECT id_favorit, judul, timestamp
                 FROM DAFTAR_FAVORIT
                 WHERE username = %s
-                """, [request.user.username])
+                """, [username])
             favorit_list = cursor.fetchall()
         return render(request, 'daftar_favorit.html', {'favorit_list': favorit_list})
-    else:
-        return render(request, 'daftar_favorit.html', {'user': request.user})
     
 
 def detail_favorit(request, favorit_id):
-    if request.user.is_authenticated:
-        username = request.user.username
+        username = request.COOKIES["username"]
         with connection.cursor() as cursor:
             cursor.execute("""
                 SELECT t.judul, t.id_tayangan
@@ -41,19 +37,16 @@ def detail_favorit(request, favorit_id):
                 """, [favorit_id])
             tayangan_list = cursor.fetchall()
         return render(request, 'detail_favorit.html', {'tayangan_list': tayangan_list, 'favorit_id': favorit_id, 'user': request.user})
-    else:
-        return redirect('login')
     
 
 def hapus_favorit(request, favorit_id):
-    if request.user.is_authenticated:
-        username = request.user.username
+        username = request.COOKIES["username"]
         try:
             with connection.cursor() as cursor:
                 cursor.execute("""
                     DELETE FROM DAFTAR_FAVORIT
                     WHERE id_favorit = %s AND username = %s
-                """, [favorit_id, request.user.username])
+                """, [favorit_id, username])
             return JsonResponse({'success': True})
         except Exception as e:
             return JsonResponse({'success': False, 'message': str(e)})
